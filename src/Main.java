76ee5bd695789d53a17037f8ed7f17427be75950
Alexandre Florentin <lexfloren@gmail.com>
2015-04-30 16:36:35 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 
@@ -15,17 +15,20 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreePath;
 
 /**
- * Cette classe dÈmarre le programme et son interface graphique.
- * <br>Il n'est donc pas Ètonnant d'y retrouver de nombreux ÈlÈments servant uniquement ‡ l'interface graphique.
- * <br>J'essaie nÈanmoins de regrouper tout l'aspect graphique vers le bas du fichier autant que possible.
+ * Cette classe d√©marre le programme et son interface graphique.
+ * <br>Il n'est donc pas √©tonnant d'y retrouver de nombreux √©l√©ments servant uniquement √† l'interface graphique.
+ * <br>J'essaie n√©anmoins de regrouper tout l'aspect graphique vers le bas du fichier autant que possible.
  * @author Alexandre Florentin
  *
  */
 public class Main {
+	public static JRadioButton radio0;
+	public static JRadioButton radio1;
+	public static JRadioButton radio2;
+	static Thread t;
 	static String source = "";
 	public static ArrayList<Recuperation> royaumes;
 	public static JFrame frame;
@@ -41,10 +44,11 @@ public class Main {
 	public static JPopupMenu menuContextuel;
 	public static ArrayList<JCheckBoxMenuItem> checkKingdom;
 	public static JCheckBoxMenuItem sauvegarder;
+	public static JCheckBoxMenuItem checkThread;
 	
 	public static void main(String[] args) {
 		initialize();
-		royaumes = new ArrayList<Recuperation>();
+		royaumes = new ArrayList<Recuperation>(3);
 		royaumes.add(new Recuperation("Eukaryotes"));
 		royaumes.add(new Recuperation("Prokaryotes"));
 		royaumes.add(new Recuperation("Virus"));
@@ -54,68 +58,115 @@ public class Main {
 		if(arborescence.getLastSelectedPathComponent() != null){
 			File dir = new File(arborescence.getLastSelectedPathComponent().toString());
 			try{
-				Desktop.getDesktop().open(dir);
+				if(dir.isDirectory()){
+					if(new File(arborescence.getLastSelectedPathComponent().toString() + ".xls").exists())
+						Desktop.getDesktop().open(new File(arborescence.getLastSelectedPathComponent().toString() + ".xls"));
+					else if(dir.getName().equals("Kingdom")){
+						Desktop.getDesktop().open(dir);
+					}
+				}else{
+					Desktop.getDesktop().open(dir);
+				}
+				
 			}catch(Exception ex){
-				System.out.println("Áa marche pas");
+				ex.printStackTrace();
 			}
 		}
 	}
 	
+	public static boolean sauvegarder(){
+		return sauvegarder.getState();
+	}
+	
+	public static boolean multiThread(){
+		return checkThread.getState();
+	}
+	
 	/**
-	 * Actions effectuÈes au clic du bouton Lancer
+	 * Actions effectu√©es au clic du bouton Lancer
 	 */
 	public static void actionsBoutonLancer(){
-		btnLancer.setEnabled(false);
 		btnVerif.setEnabled(false);
 		frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 		if(checkKingdom.get(0).isSelected()){
-			royaumes.get(0).recupFastaLocal(sauvegarder.isSelected());
+			if(radio0.isSelected())
+				royaumes.get(0).recupFastaLocal();
+			else{
+				if(radio1.isSelected())
+					Statistiques.hierarchicalStats(new File("./Kingdom/Eukaryotes"),false);
+				else
+					Statistiques.hierarchicalStats(new File("./Kingdom/Eukaryotes"),true);
+			}
 		}
 		if(checkKingdom.get(1).isSelected()){
-			royaumes.get(1).recupFastaLocal(sauvegarder.isSelected());
+			if(radio0.isSelected())
+				royaumes.get(1).recupFastaLocal();
+			else{
+				if(radio1.isSelected())
+					Statistiques.hierarchicalStats(new File("./Kingdom/Prokaryotes"),false);
+				else
+					Statistiques.hierarchicalStats(new File("./Kingdom/Prokaryotes"),true);
+			}
 		}
 		if(checkKingdom.get(2).isSelected()){
-			royaumes.get(2).recupFastaLocal(sauvegarder.isSelected());
+			if(radio0.isSelected())
+				royaumes.get(2).recupFastaLocal();
+			else{
+				if(radio1.isSelected())
+					Statistiques.hierarchicalStats(new File("./Kingdom/Virus"),false);
+				else
+					Statistiques.hierarchicalStats(new File("./Kingdom/Virus"),true);
+			}
+			
 		}
 		frame.setCursor(Cursor.getDefaultCursor());
 		btnVerif.setEnabled(true);
-		btnLancer.setEnabled(true);
 		refreshArborescence();
-		progressText("Statistiques terminÈes !");
+		progressText("Statistiques termin√©es !");
+		btnLancer.setText("Lancer");
 	}
 	
 	public static void actionsBoutonVerif(){
 		btnLancer.setEnabled(false);
 		btnVerif.setEnabled(false);
-		progressText("VÈrification de l'arborescence");
+		progressText("V√©rification de l'arborescence");
 		frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
 
+		
 		int modifications=0;
-		if(checkKingdom.get(0).isSelected())	modifications += royaumes.get(0).verifierInfos();
-		if(checkKingdom.get(1).isSelected())	modifications += royaumes.get(1).verifierInfos();
-		if(checkKingdom.get(2).isSelected())	modifications += royaumes.get(2).verifierInfos();
+		if(checkKingdom.get(0).isSelected()){
+			modifications += royaumes.get(0).verifierInfos();
+		}
+		if(checkKingdom.get(1).isSelected()){
+			modifications += royaumes.get(1).verifierInfos();
+		}
+		if(checkKingdom.get(2).isSelected()){
+			modifications += royaumes.get(2).verifierInfos();
+		}
 		
 		frame.setCursor(Cursor.getDefaultCursor());
 		btnVerif.setEnabled(true);
 		btnLancer.setEnabled(true);
 		refreshArborescence();
-		Main.progressText("VÈrification effectuÈe. "+modifications+" modifications trouvÈes");
+		Main.progressText("Votre arborescence est √† jour.");//+modifications+" modifications trouv√©es");
 		Main.progress(100);
 	}
 	
 	/**
-	 * RafraÓchissement de l'arborescence affichÈe
+	 * Rafra√Æchissement de l'arborescence affich√©e
 	 */
 	static void refreshArborescence(){
 		root = getSubDirs(new File("./Kingdom/"));
 		arborescence = new JTree(root,true);
-		arborescence.setRootVisible(false);
+		arborescence.setRootVisible(true);
+		
 		arborescence.addMouseListener(new MouseListener(){
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				if(e.getButton() == MouseEvent.BUTTON1){
 					if(e.getClickCount() == 2)
 						ouvrirDossier();
+					
 				}
 			}
 			@Override
@@ -135,20 +186,21 @@ public class Main {
 	}
 	
 	/**
-	 * CrÈation de toute l'interface graphique
+	 * Cr√©ation de toute l'interface graphique
 	 */
 	static void initialize(){
 		try {
-            // Set System L&F
-			/*UIManager.setLookAndFeel(
-					UIManager.getSystemLookAndFeelClassName());*/
 			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 		} 
 		catch (Exception e) {
 			// handle exception
 		}
 		frame = new JFrame();
-		frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Fenetre.class.getResource("/javax/swing/plaf/metal/icons/ocean/hardDrive.gif")));
+		try{
+			frame.setIconImage(ImageIO.read(new File("res/icone.png")));
+		}catch(IOException e){
+			System.err.println("Ic√¥ne introuvable");
+		}
 		frame.setTitle("BioInfo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100, 100, 650, 500);
@@ -176,11 +228,15 @@ public class Main {
 					System.exit(0);
 				}});
 		
-		JMenu mnEdition = new JMenu("\u00C9dition");
-		menuBar.add(mnEdition);
+		JMenu parametres = new JMenu("Param√®tres");
+		menuBar.add(parametres);
 		
-		JMenu mnArbre = new JMenu("Arbre");
-		menuBar.add(mnArbre);
+		checkThread = new JCheckBoxMenuItem("Multi-thread");
+		checkThread.setSelected(false);
+		parametres.add(checkThread);
+		
+		JMenu mnArbre = new JMenu("Royaumes concern√©s");
+		parametres.add(mnArbre);
 		
 		JMenu mnAffichage = new JMenu("Affichage");
 		menuBar.add(mnAffichage);	
@@ -198,12 +254,26 @@ public class Main {
 		checkKingdom.get(2).setSelected(true);
 		mnArbre.add(checkKingdom.get(2));
 	
-		sauvegarder = new JCheckBoxMenuItem("Sauvegarder les sÈquences");
+		sauvegarder = new JCheckBoxMenuItem("Sauvegarder les s√©quences");
 		sauvegarder.setSelected(false);
-		mnArbre.add(sauvegarder);
+		parametres.add(sauvegarder);
 		
-		JMenu mnOutils = new JMenu("Outils");
-		menuBar.add(mnOutils);
+		JMenu stats = new JMenu("Statistiques");
+		radio0 = new JRadioButton("Organisme uniquement");
+		radio0.setSelected(true);
+		radio1 = new JRadioButton("Statistique fine");
+		radio2 = new JRadioButton("Statistique massive");
+		ButtonGroup btnGrp = new ButtonGroup();
+		btnGrp.add(radio0);
+		btnGrp.add(radio1);
+		btnGrp.add(radio2);
+		parametres.addSeparator();
+		
+		stats.add(radio0);
+		stats.add(radio1);
+		stats.add(radio2);
+		parametres.add(stats);
+		
 		//FIN BARRE DU HAUT
 		
 		
@@ -271,15 +341,25 @@ public class Main {
 		btnLancer = new JButton("Lancer");
 		panel_1.add(btnLancer);
 		btnLancer.addActionListener(new ActionListener(){
+			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent arg0) {
-				//On va lancer la recherche en ligne
-				Runnable runner = new Runnable(){
-			        public void run() {
-			        	actionsBoutonLancer();
-			        }
-			    };
-			    Thread t = new Thread(runner, "Code Executer");
-			    t.start();
+				if(btnLancer.getText().equals("Lancer")){
+					btnLancer.setText("Interrompre");
+					//On va lancer la recherche en ligne
+					Runnable runner = new Runnable(){
+				        public void run() {
+				        	actionsBoutonLancer();
+				        }
+				    };
+				    t = new Thread(runner, "Code Executer");
+				    t.start();
+				}else{
+					t.stop();
+					frame.setCursor(Cursor.getDefaultCursor());
+					progressText("T√¢che interrompue");
+					progress(100);
+					btnLancer.setText("Lancer");
+				}
 			}});
 		
 		btnVerif = new JButton("Verifier");
@@ -340,7 +420,6 @@ public class Main {
 	public static DefaultMutableTreeNode getSubDirs(File root){
 		DefaultMutableTreeNode racine = new DefaultMutableTreeNode(root,true);
 		File[] list = root.listFiles();
-		boolean hasLog=false;
 		if(list!= null){
 			for (int j = 0 ; j<list.length ; j++){
 				DefaultMutableTreeNode file = null;
@@ -348,10 +427,10 @@ public class Main {
 					file = getSubDirs(list[j]);  
 					racine.add(file);
 				}else if(list[j].isFile()){
-					if(list[j].getName().equals("log.txt"))
-						hasLog = true;
-					else if(list[j].getName().equals("content.txt")){
+					if(list[j].getName().equals("content.txt")){
 						//Skip
+					}else if(list[j].getName().endsWith(".xls")){
+						
 					}else{
 						file = new DefaultMutableTreeNode(list[j]);
 						file.setAllowsChildren(false);
@@ -360,12 +439,6 @@ public class Main {
 				}
 			}
 		}
-		if(!racine.isLeaf()){
-			if(racine.getFirstChild().isLeaf() && !hasLog){
-			racine.remove(0);
-			}
-		}
 		return racine;
 	}
 }
-
